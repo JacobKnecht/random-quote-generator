9ee413771091c657c1d5f8ca0f4907c4bf7b48a2
Jacob Knecht <jacobknecht3@gmail.com>
2019-03-05 21:39:14 -0600

    added comments and made minor edits to script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
+/*
+* Jacob Knecht
+* Treehouse Full-Stack JavaScript Development Techdegree
+* Project 1 - Random Quote Generator
+*/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/***
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
+/*
+* This creates an array of quote objects (10 in total)
+* each quote object contains a quote, the source of the quote, the quote's
+* citation, the year of the quote's origin, and tags to categorize the quote
+*/
 var quotes = [
   {
     quote:"We all make choices, but in the end, our choices make us.",
     source:'Andrew Ryan',
     citation:'Bioshock',
-    year:'2007',
+    year:'(2007)',
     tags:"'iconic'"
   },
   {
@@ -27,7 +22,7 @@ var quotes = [
       "they are conscious of saying.",
     source:'Albert Camus',
     citation:'The Myth of Sisyphus',
-    year:'1942',
+    year:'(1942)',
     tags:"'wisdom'"
   },
   {
@@ -35,7 +30,7 @@ var quotes = [
       "that killed the beast!",
     source:'Carl Denham',
     citation:'King Kong',
-    year:'1933',
+    year:'(1933)',
     tags:"'iconic'"
   },
   {
@@ -43,7 +38,7 @@ var quotes = [
       "against living things.",
     source:'Barry Burton',
     citation:'Resident Evil',
-    year:'1996',
+    year:'(1996)',
     tags:"'humor'"
   },
   {
@@ -51,7 +46,7 @@ var quotes = [
       "pursue them.",
     source:'Walt Disney',
     citation:'How to Be Like Walt: Capturing the Magic Every Day of Your Life',
-    year:'2004',
+    year:'(2004)',
     tags:"'inspirational'"
   },
   {
@@ -59,29 +54,29 @@ var quotes = [
       "I am a game developer. But in my heart, I am a gamer.",
     source:'Satoru Iwata',
     citation:'2005 GDC Keynote',
-    year:'2005',
+    year:'(2005)',
     tags:"'passion'"
   },
   {
     quote:"Whoever saves one life, saves the world entire.",
     source:'Itzhak Stern',
     citation:"Schindler's List",
-    year:'1993',
+    year:'(1993)',
     tags:"'kindness'"
   },
   {
     quote:"WITH GREAT POWER THERE MUST ALSO COME--GREAT RESPONSIBILITY!",
     source:'Stan Lee',
     citation:'Amazing Fantasy #15',
-    year:'1962',
+    year:'(1962)',
     tags:"'inspirational'"
   },
   {
-    quote:"The heart of Man is not compound of lies," +
+    quote:"The heart of Man is not compound of lies, " +
       "but draws some wisdom from the only Wise.",
     source:'J.R.R. Tolkien',
     citation:'Mythopoeia',
-    year:'1931',
+    year:'(1931)',
     tags:"'wisdom'"
   },
   {
@@ -89,51 +84,25 @@ var quotes = [
       "'Suffer now, and live the rest of your life as a champion'.",
     source:'Muhammad Ali',
     citation:'"Ali: Born Again!" Newsweek',
-    year:'1978',
+    year:'(1978)',
     tags:"'inspirational'"
   },
 ]
 
-//test the array 'quotes' in the console
-console.log(quotes);
-
-//function to generate a random index value between 0 and the length of the
-//array of quotes
+//creates a function to generate a random index value between 0 and the length
+// of the array of quotes - not required but a good idea for a function
 function getRandomIndex () {
   return Math.floor(Math.random() * quotes.length);
 }
 
-//test the function getRandomIndex() in the console
-console.log(getRandomIndex());
-
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
-
+//creates a function to pick a random quote from the array of quotes
 function getRandomQuote () {
   var randomNum = getRandomIndex();
   return quotes[randomNum];
 }
 
-//test the function getRandomQuote() in the console
-console.log(getRandomQuote());
-
-
-/***
-  Create the `printQuote` function to:
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string.
-***/
-
+//creates a function to generate the necessary HTML to print the quote to
+//the web browser
 function printQuote () {
   var randomQuote = getRandomQuote();
   var html = '';
@@ -162,15 +131,5 @@ function printQuote () {
   document.getElementById('quote-box').innerHTML = html;
 }
 
-
-/***
-  When the "Show another quote" button is clicked, the event listener
-  below will be triggered, and it will call, or "invoke", the `printQuote`
-  function. So do not make any changes to the line of code below this
-  comment.
-***/
-
+//this function provides functionality to the 'Show another quote' button
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
